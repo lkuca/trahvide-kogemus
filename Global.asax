9b384fc38524cbaecccb8514ece36d2755537547
opilane <opilane@HP-CZC2349HTQ>
2024-08-30 13:54:54 +0300

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="trahvide_kogemus.MvcApplication" Language="C#" %>
---
+++ Models/trahvide_eelvade.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace trahvide_kogemus.Models
+{
+    public class trahvide_eelvade
+    {
+        [Required(ErrorMessage = "Sissesta nimi")]
+        public string Nimi { get; set; }
+        [Required(ErrorMessage = "sissesta Email")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "valesti sissestatud email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Sissesta isikukoodi numbrid")]
+        public int isikukood { get; set; }
+        [Required(ErrorMessage = "Sisesta oma pin")]
+
+        public int pin { get; set; }
+    }
+
+}
